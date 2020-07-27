608c17cdbadb33377a4a2f5a4d9ecce95c8f7c86
but-sergey <but-sergey@bk.ru>
2020-07-27 16:09:38 +0300

    Задание 1-2

---
+++ Lesson01/hw1-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** args)
+{
+    cout << "Sample string \n\t Sample string 2" << endl << "===" << endl;
+    cout << "1\r2\r3" << endl << "===" << endl;
+    cout << "1\t2\t3" << endl << "===" << endl;
+    cout << "1\n2\n3" << endl << "===" << endl;
+
+    return 0;
+}
