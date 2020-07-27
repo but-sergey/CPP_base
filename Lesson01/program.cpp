2b71f957b1008a7326f4238f08a38416a2a9e58c
but-sergey <but-sergey@bk.ru>
2020-07-27 14:28:05 +0300

    Первая программа

---
+++ Lesson01/program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define IT_WORKS "It works!"
+
+using namespace std;
+
+int main(int argc, char** args)
+{
+    const int SUCCESS = 0;
+    cout << IT_WORKS << endl;
+    return SUCCESS;
+}
