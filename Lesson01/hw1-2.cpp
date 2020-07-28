3436b1bdf1cc934e01219fe4e444072f6c68875b
but-sergey <but-sergey@bk.ru>
2020-07-28 09:34:57 +0300

    Рефакторинг hw1-2

--- Lesson01/hw1-2.cpp
+++ Lesson01/hw1-2.cpp
@@ -4,10 +4,21 @@ using namespace std;
 
 int main(int argc, char** args)
 {
-    cout << "Sample string \n\t Sample string 2" << endl << "===" << endl;
-    cout << "1\r2\r3" << endl << "===" << endl;
-    cout << "1\t2\t3" << endl << "===" << endl;
-    cout << "1\n2\n3" << endl << "===" << endl;
+    // Задание 2.1
+    cout << "Sample string \n\t Sample string 2";
+    cout << endl << "===" << endl;
+
+    // \r - возврат в начало строки
+    cout << "1\r2\r3";
+    cout << endl << "===" << endl;
+    
+    // \t - вставка табуляции
+    cout << "1\t2\t3";
+    cout << endl << "===" << endl;
+
+    // \n - перевод на новую строку
+    cout << "1\n2\n3";
+    cout << endl << "===" << endl;
 
     return 0;
 }
