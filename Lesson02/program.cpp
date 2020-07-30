43f74a334753d1f8ba9a9ad7cb35d998d98598a3
but-sergey <but-sergey@bk.ru>
2020-07-30 16:35:46 +0300

    Программа с лекции 2-4

--- Lesson02/program.cpp
+++ Lesson02/program.cpp
@@ -3,15 +3,20 @@
 
 using namespace std;
 
+void PrintVal(int a, unsigned long b)
+{
+    cout << dec << a << endl;
+    cout << hex << &a << endl;
+    cout << dec << b << endl;
+    cout << hex << &b << endl;
+}
+
 int main(int argc, char** args)
 {
     int a  = 264000;
     unsigned long b = 123456789;
 
-    cout << dec << a << endl;
-    cout << hex << &a << endl;
-    cout << dec << b << endl;
-    cout << hex << &b << endl;
+    PrintVal(a, b);
 
     return 0;
 }
