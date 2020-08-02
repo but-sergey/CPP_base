a8e12b4c73facc095ae86d02ed7e5c8aa7331d7a
but-sergey <but-sergey@bk.ru>
2020-08-02 18:21:54 +0300

    Урок 3-2: добавили ветку else

--- Lesson03/program2.cpp
+++ Lesson03/program2.cpp
@@ -7,9 +7,11 @@ int main(int argc, char** args)
     cout << "Enter num ";
     int a;
     cin >> a;
-    
+
     if(a > 10)
         cout << "a > 10" << endl;
+    else
+        cout << "a < 10" << endl;
 
     return 0;
 }
