363641597e8d4b1976b6cd61ac24db2f2b859a04
but-sergey <but-sergey@bk.ru>
2020-08-02 17:56:55 +0300

    Домашнее задание 3-1 выполнено

---
+++ Lesson03/hw3-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "hw3-1.h"
+
+using namespace std;
+using namespace _geek;
+
+int main(int argc, char** args)
+{
+    int a = 5;
+    int b = 7;
+
+    cout << add(a, b) << endl;
+
+    return 0;
+}
