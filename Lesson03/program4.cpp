b3724bb5524cd8dce1f772b30752396e915a4d54
but-sergey <but-sergey@bk.ru>
2020-08-02 19:38:20 +0300

    Урок 3-4: циклы с предусловием и с постусловием

---
+++ Lesson03/program4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** args)
+{
+    int a = 2;
+    
+    while(a < 10) a++;
+
+    do
+    {
+        cout << a++ << endl;
+    } while (a >= 1);
+
+    return 0;
+}
