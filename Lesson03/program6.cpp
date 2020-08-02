cb4af1757ee2508b1ffbf1074ccfb207585a682a
but-sergey <but-sergey@bk.ru>
2020-08-02 21:50:17 +0300

    Урок 3-6: заполнение и вывод массива

--- Lesson03/program6.cpp
+++ Lesson03/program6.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main(int argc, char** args)
 {
+    int ar[10];
+
     for(int i = 0; i < 10; i++)
     {
-        cout << i << endl;
+        ar[i] = i;
+        cout << ar[i] << endl;
     }
 
     return 0;
