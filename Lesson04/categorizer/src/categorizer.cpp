54be90cf62bfe8a8816010e60d1c557e4176d328
but-sergey <but-sergey@bk.ru>
2020-08-04 16:39:10 +0300

    Урок 4-2: функции доступа к файлам

--- Lesson04/categorizer/src/categorizer.cpp
+++ Lesson04/categorizer/src/categorizer.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main(int argc, char** args)
 {
+    fstream f;
+
+    if(argc == 1)
+    {
+        cout << "Please specify input file(s)!" << endl;
+        return 1;
+    }
+
+    for(int i = 1; i < argc; i++)
+    {
+        f.open(args[i], ios::binary | ios::in);
+        if(!f.is_open())
+        {
+            cout << "File: " << args[i] << " is not accessible!" << endl;
+            return 1;
+        }
+        f.close();
+    }
 
     return 0;
 }
