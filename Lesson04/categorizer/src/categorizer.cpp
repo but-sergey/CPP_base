8cf6a22a6cf88a45446a853e2e5640db78b10dd4
but-sergey <but-sergey@bk.ru>
2020-08-05 10:08:55 +0300

    Урок 4-3: чтение заголовка файла

--- Lesson04/categorizer/src/categorizer.cpp
+++ Lesson04/categorizer/src/categorizer.cpp
@@ -2,9 +2,12 @@
 
 using namespace std;
 
+unsigned int histogram[256];
+
 int main(int argc, char** args)
 {
     fstream f;
+    char PGM_hdr1[2];
 
     if(argc == 1)
     {
@@ -18,8 +21,17 @@ int main(int argc, char** args)
         if(!f.is_open())
         {
             cout << "File: " << args[i] << " is not accessible!" << endl;
-            return 1;
+            continue;
         }
+        cout << "File " << args[i] << " opened" << endl;
+        
+        f.read(PGM_hdr1, 2);
+        if((PGM_hdr1[0] != 0x50) || (PGM_hdr1[1] != 0x35))
+            continue;
+        cout << "PGM header detected" << endl;
+
+        f.seekg((long int)f.tellg() + 1);
+
         f.close();
     }
 
